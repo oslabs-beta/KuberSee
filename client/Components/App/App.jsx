c4fe0c24f65905ce169cd271d511edcf76852ec0
Jordan Lopez <jordanlopez992@gmail.com>
2023-07-08 18:48:31 -0400

    Establish React Router

--- client/Components/App/App.jsx
+++ client/Components/App/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "../../Pages/NotFound/NotFound.jsx";
 import HomePage from "../../Pages/Home/Homepage.jsx";
 import LoginPage from '../../Pages/Login/LoginPage.jsx';
 import SignupPage from "../../Pages/Signup/Signuppage.jsx";
 import NavBar from "../NavBar/NavBar.jsx";
+
 const App = () => {
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        hello
-      </h1>
-      <LoginPage/>
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 };
 export default App;
