57b9ca2b3448d429b90c7fe3be5b5ac12059a953
Daniel Zhao <dzhao1880@gmail.com>
2023-07-08 16:05:51 -0400

    login page frontend

--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './Components/App/App.jsx'
+import React from "react";
+import { render } from "react-dom";
+import * as ReactDOM from "react-dom";
+import App from "./Components/App/App.jsx";
 
-render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <App />
+  // document.getElementById('root')
 );
