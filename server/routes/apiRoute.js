22f7c0899ff69092f8bd2502908a7f135029e714
Jordan Lopez <jordanlopez992@gmail.com>
2023-07-10 12:41:57 -0400

    Establish log api structure

--- server/routes/apiRoute.js
+++ server/routes/apiRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const stream = require('stream');
 const k8s = require('@kubernetes/client-node');
 const router = express.Router();
 
@@ -11,11 +12,11 @@ router.get('/metrics', async (req, res) => {
     
     const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
     const metricsClient = new k8s.Metrics(kc);
-    const namespace = "default";
+    const namespace = "kube-system";
     
-    let topNodes = [];
-    let topPods = [];
-    let namespaces = [];
+    const topNodes = [];
+    const topPods = [];
+    const namespaces = [];
     
     await k8s.topNodes(k8sApi, metricsClient, namespace)
         .then((nodes) => {
@@ -46,9 +47,9 @@ router.get('/metrics', async (req, res) => {
         });
     
     await k8sApi.listNamespace()
-        .then((res) => {
-            for (let i in res.body.items) {
-                namespaces.push(res.body.items[i].metadata.name);
+        .then((data) => {
+            for (let i in data.body.items) {
+                namespaces.push(data.body.items[i].metadata.name);
             }
         });
 
@@ -59,6 +60,60 @@ router.get('/metrics', async (req, res) => {
     });
 });
 
+router.get('/logs', async (req, res) => {
+    try {
+        const kc = new k8s.KubeConfig();
+        kc.loadFromDefault();
+        
+        const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+      const namespace = "kube-system";
+      const podName = 'kube-controller-manager-minikube';
+      const containerName = "";
+      const tailLines = 100; // Number of lines to fetch
+  
+      const logsResponse = await k8sApi.readNamespacedPodLog(podName, namespace, containerName, undefined, undefined, undefined, tailLines);
+      const data = logsResponse.body;
+  
+        const logs = data.split('\n')
+      return res.status(200).json({logs});
+    } catch (error) {
+      console.error("Error fetching logs:", error);
+      return res.status(500).send("Error fetching logs");
+    }
+  });
 
+// router.get('/logs', async (req, res) => {
+//     const kc = new k8s.KubeConfig();
+//     kc.loadFromDefault();
+//     const namespace = "kube-system";
+
+//     const log = new k8s.Log(kc);
+//     const logs = [];
+//     const logStream = new stream.PassThrough();
+
+//     await logStream.on('data', (chunk) => {
+//         // use write rather than console.log to prevent double line feed
+//         // process.stdout.write(chunk);
+//         const log = chunk.toString();
+//         console.log(log)
+//         logs.push({log: chunk.toString()});
+//     });
+    
+//     await log.log(namespace, 'kube-controller-manager-minikube', '', logStream, {follow: true, tailLines: 50, pretty: false, timestamps: false})
+//     .catch(err => {
+//             console.log(err);
+//             process.exit(1);
+//     })
+//     .then(req => {
+//         // disconnects after 5 seconds
+//         if (req) {
+//             setTimeout(function() {
+//                 req.abort();
+//             }, 5000);
+//         }
+//     });
+//     console.log(logs);
+//     return res.status(200).json({Logs: logs});
+// });
 
 module.exports = router;
