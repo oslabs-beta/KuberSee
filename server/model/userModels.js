f3a45090f05046383c6da5000d5a0706507f3a2b
Daniel Zhao <dzhao1880@gmail.com>
2023-07-11 12:20:43 -0400

    add backend route for signin

---
+++ server/model/userModels.js
@@ -0,0 +1,19 @@
+// import * as dotenv from "dotenv";
+const { Client } = require("pg");
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on("connect", (client) => {
+  client.query("SET DATESTYLE = iso, mdy");
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    // console.log("executed query", text);
+    return pool.query(text, params, callback);
+  },
+};
