60ebf6a70f7d924d626867402526b5cf0b5e8f07
Jordan Lopez <jordanlopez992@gmail.com>
2023-07-20 11:58:16 -0400

    refresh works

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 const PORT = 3000;
-const apiRoute = require('./routes/apiRoute');
-const authRoute = require('./routes/authRoute');
-const http = require('http');
+const apiRoute = require("./routes/apiRoute");
+const authRoute = require("./routes/authRoute");
+const http = require("http");
 // const { Server } = require('socket.io');
 // const cookieParser = require("cookie-parser");
 // const sessions = require("express-session");
@@ -26,9 +26,17 @@ app.use(cors());
 //   })
 // );
 
-app.use('/', express.static(path.resolve(__dirname, '../build')));
-app.use('/api', apiRoute);
-app.use('/auth', authRoute);
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.use("/", express.static(path.resolve(__dirname, "../build")));
+app.use("/api", apiRoute);
+app.use("/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}... idea generator app`);
