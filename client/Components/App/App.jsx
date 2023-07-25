9056e7f245281e4170b5b93f395eeb5427c5e489
Daniel Zhao <dzhao1880@gmail.com>
2023-07-25 17:02:43 -0400

    db-test file

--- client/Components/App/App.jsx
+++ client/Components/App/App.jsx
@@ -10,23 +10,32 @@ import io from 'socket.io-client'
 const socket = io.connect("http://localhost:3000");
 
 const App = () => {
-  const location = useLocation();
+  // const location = useLocation();
   useEffect(() => {
     socket.emit('stats', null);
   }, []);
-  const showNavBar = location.pathname === '/' || location.pathname === '/signup';
+  // const showNavBar = location.pathname === '/' || location.pathname === '/signup';
   return (
     // <BrowserRouter>
-    // <BrowserRouter>
-    <div>
-      {showNavBar && <NavBar />}
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='home' element={<HomePage socket={socket} />} />
         <Route path='signup' element={<SignupPage />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
-      </div>
+    </BrowserRouter>
+
+
+    // <div>
+    //   {showNavBar && <NavBar />}
+    //   <Routes>
+    //     <Route path='/' element={<LoginPage />} />
+    //     <Route path='home' element={<HomePage socket={socket} />} />
+    //     <Route path='signup' element={<SignupPage />} />
+    //     <Route path='*' element={<NotFound />} />
+    //   </Routes>
+    // </div>
     // </BrowserRouter>
     // </BrowserRouter>
   );
