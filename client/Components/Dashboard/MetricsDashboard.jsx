2866d9ccbcfbf3c92976a4bd54136bc15e98b31f
inorle <elinorweissberg@gmail.com>
2023-07-26 13:28:08 -0400

    deleted files that we aren't using

--- client/Components/Dashboard/MetricsDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import GraphChart from '../Charts/GraphChart.jsx'
-import LineChart from '../Charts/LineChart.jsx'
-export default function MetricsDashboard() {
-  return (
-    <div>MetricsDashboard</div>
-  )
-}
