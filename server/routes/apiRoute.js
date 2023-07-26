5f5ffcf84dcd6a8822491b5b1b35e73b4e0f77b5
Jordan Lopez <jordanlopez992@gmail.com>
2023-07-26 11:43:38 -0400

    remove http endpoints

--- server/routes/apiRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const apiController = require("../controllers/apiController");
-
-router.get("/logs/:namespace/:podname", apiController.getLogs, (req, res) => {
-  return res.status(200).json(res.locals.logs);
-});
-
-module.exports = router;
