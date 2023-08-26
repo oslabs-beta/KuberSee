dbf17aad7d7fe7beb980fa8896236381c6adbc70
joey-cheng-codes <j.cheng93@gmail.com>
2023-08-26 00:31:47 -0400

    installed eslint for typescript fixed extra comma on package.json created a nvmrc file

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,64 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  entry: "./client/index.js",
-  // target: 'electron-renderer',
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./electron/build"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "index.html"),
-    }),
-  ],
-  performance: {
-    hints: false,
-  },
-  devServer: {
-    historyApiFallback: true,
-    port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        router: () => "http://localhost:3000",
-      },
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: "ts-loader",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg|svg|gif)$/,
-        loader: "file-loader",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".jsx", ".js"],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+	mode: process.env.NODE_ENV || 'development',
+	entry: './client/index.js',
+	// Target: 'electron-renderer',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, './electron/build'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, 'index.html'),
+		}),
+	],
+	performance: {
+		hints: false,
+	},
+	devServer: {
+		historyApiFallback: true,
+		port: 8080,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080',
+				router: () => 'http://localhost:3000',
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				use: 'ts-loader',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|jpg|jpeg|svg|gif)$/,
+				loader: 'file-loader',
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.jsx', '.js'],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
 };
